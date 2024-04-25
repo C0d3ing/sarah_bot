8dda6e2a3c91125fc0be6271f75ec2897841608c
Roman <46465589+C0d3ing@users.noreply.github.com>
2024-04-25 15:41:45 -0700

    Anem updates

--- MATLAB/CSV_reader_and_analysis.m
+++ MATLAB/CSV_reader_and_analysis.m
@@ -54,8 +54,24 @@ temperatures = (therm*3.3/1024)*13.569 - 18.182;
 % salinity calibration
 salinity = (((salmax-salmin)*3.3/1024*1000)-104)/20.9;
 
-% anemometer calibration
-% windspeed = ;
+%% anemometer calibration
+
+%find the time rising edges occure
+risEdgetime = [];
+for i = 2:length(anem)
+    if (anem(i-1) == 0 && anem(i)>0)
+        risEdgetime = [risEdgetime time(i)];
+    end
+end
+
+%Find the periods between rising edges
+anemPeriods = [];
+for i = 1:(length(risEdgetime)-1)
+    anemPeriods = [anemPeriods (risEdgetime(i+1) - risEdgetime(i))];
+end
+
+%convert periods using calibration curve
+windspeed = anemPeriods * -8.03 +8.74;
 
 % plot data
-plot(time, temperatures);
+plot(time, windspeed);
